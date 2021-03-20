6ed78860eec854504ebd8ed463210ba71f91fc1b
Morgaine B <morgainebowers@gmail.com>
2021-03-20 18:41:36 -0400

    set up import & reader, calcualted votes

---
+++ PyPoll/main.py
@@ -0,0 +1,30 @@
+#import os to make code platform agnostic
+import os
+
+#import csv to read csv file
+import csv
+
+#create a path to the csv file
+csvpath = os.path.join("Resources", "election_data.csv")
+
+#read the csv file and construct csv reader, skip the header
+with open(csvpath, 'r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter = ",")
+    csv_header = next(csv_reader)
+
+    #define the lists to store data
+    votes = []
+    candidates = []
+    
+    #calculate the total number of votes cast
+    for row in csv_reader:
+        votes.append(row[0])
+        total_votes = len(votes)
+
+
+#Print analysis to Terminal
+print("Election Results")
+print("------------------")
+print("Total Votes ", total_votes)
+
+      
