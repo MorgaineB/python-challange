0e47cf7ebf65375ca3e4d4f24ee607e06e342344
Morgaine B <morgainebowers@gmail.com>
2021-03-20 15:34:40 -0400

    Calculated the total number of months

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,3 +12,26 @@ with open(csvpath, 'r') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ',')
 
 
+#Calculate the total number of months included in the dataset
+    lines = len(list(csv_reader))
+    print(lines)
+
+#Calculate the net total amount of "Profit/Losses" over the entire period
+
+
+#Calculate the changes in "Profit/Losses" over the entire period
+
+
+#Find the average "Profit/Losses" change
+
+
+#Calculate the greatest increase in profits (date and amount) over the entire period
+
+
+#Calculate the greatest decrease in profits (date and amount) over the entire peiod
+
+
+#Print analysis to Terminal
+
+
+#Export analysis to a text file
