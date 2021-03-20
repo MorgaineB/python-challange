8b9df7b8866abd5ff511b33311d0e42428c2b55c
Morgaine B <morgainebowers@gmail.com>
2021-03-20 19:34:35 -0400

    starting over

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,19 +12,30 @@ with open(csvpath, 'r') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ",")
     csv_header = next(csv_reader)
 
-    #define the lists to store data
+    #define the lists or dictionaries to store data
     votes = []
     candidates = []
-    
+
     #calculate the total number of votes cast
     for row in csv_reader:
         votes.append(row[0])
         total_votes = len(votes)
+
+        #create a complete list of candidates 
+        if row[2] not in candidates:
+            candidates.append(row[2])
+        
+    print(candidates)
+
+    #calculate the percentage of votes each candidate won
+ 
 
 
 #Print analysis to Terminal
 print("Election Results")
 print("------------------")
 print("Total Votes ", total_votes)
+print("------------------")
 
-      
+
+
